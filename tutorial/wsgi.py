377d2c05e140465e9b01e86d43ceae59ad5cb679
Vitalii <fahivchik1@gmail.com>
2023-04-03 14:05:35 +0300

    one more time8

--- tutorial/wsgi.py
+++ tutorial/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for tutorial project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+path = '/home/GuruWithYou/myproject'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
 
 application = get_wsgi_application()
