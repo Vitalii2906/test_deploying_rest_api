1965b5c5551aa6a60dcc524576cc10bd5c324188
Vitalii <fahivchik1@gmail.com>
2023-04-03 12:43:23 +0300

    one more time

--- build.ps1
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# exit on error
-$ErrorActionPreference = 'Stop'
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
--- tutorial/quickstart/apps.py
+++ tutorial/quickstart/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CinemaConfig(AppConfig):
+class QuickstartConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cinema'
+    name = 'quickstart'
